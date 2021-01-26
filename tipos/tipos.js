c888d8d51c05de9f857ae1161ece4cbbff15963c
joice.pepe <joice.pepe@neomind.com.br>
2021-01-26 12:59:22 -0300

    Seção 2: Usando Tipos - Aula 13. Atribuindo Tipos Explícitos

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -1,4 +1,20 @@
 "use strict";
 //string
-var nome = 'João123';
-console.log(nome);
+var nome = 'João'; //defini o tipo dela
+console.log('String: ' + nome);
+//numbers - não diferencia números inteiros e números decimais
+var idade = 27;
+console.log('Number: ' + idade);
+//boolean
+var possuiHobbies = false;
+console.log('Booleano: ' + possuiHobbies);
+//tipos explícitos
+var minhaIdade; //não declarou a variável com um tipo explícito
+/*
+No typescript uma variável sem ter um tipo explícito é "any"
+let minhaIdade = any (está formanlisando que ela não terá um tipo específico)
+*/
+minhaIdade = 27; //então tem um tipo dinâmico
+console.log(typeof minhaIdade);
+minhaIdade = 'idade é 27';
+console.log(typeof minhaIdade);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,3 +1,23 @@
 //string
-let nome = 'João123'
-console.log(nome)
+let nome : string = 'João' //defini o tipo dela
+console.log('String: ' + nome)
+
+//numbers - não diferencia números inteiros e números decimais
+let idade = 27
+console.log('Number: ' + idade)
+
+//boolean
+let possuiHobbies = false
+console.log('Booleano: ' + possuiHobbies)
+
+//tipos explícitos
+let minhaIdade //não declarou a variável com um tipo explícito
+/*
+No typescript uma variável sem ter um tipo explícito é "any"
+let minhaIdade = any (está formanlisando que ela não terá um tipo específico)
+*/
+minhaIdade = 27 //então tem um tipo dinâmico
+console.log(typeof minhaIdade)
+
+minhaIdade = 'idade é 27'
+console.log(typeof minhaIdade)
